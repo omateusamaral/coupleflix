e4d2389e721984a2b49c0f847d21b859e53b30e0
omateusamaral <mateus.amaral018@gmail.com>
2023-08-14 22:12:41 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Coupleflix",
+export const metadata: Metadata = {
+  title: "Coupleflix - Recommendações de filmes e séries",
   description: "Recommendações de filmes e séries",
 };
 
